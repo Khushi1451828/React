6db8cfa17e8c3ca8739c8b0b707e9068ae003844
ks1451828 <ks1451828@gmail.com>
2023-12-20 20:03:14 +0530

    context API

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -19,3 +19,13 @@ function App() {
 }
 
 export default App
+
+
+//context api and state management -- 
+//to share or pass data from on component to another there will be tricky to done that 
+//so we make a global component you can say which will be the consumer of the other component's data and provide the data to the required one 
+//3 steps
+//[1]===>create a context using createContext(the global component one)
+//[2]===>provide the context ( for every component to know eveery component that there is a context with all data)
+//[3]===>consume the context in child components (consume the context to the child components whoever is need for)
+
